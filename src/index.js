e929ff09d42dc0f0ad2034bd8e108e048de7ef05
tbro199803 <165161210+tbro199803@users.noreply.github.com>
2024-06-17 12:05:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zzh1998.com": dockerHub,
+  "quay.zzh1998.com": "https://quay.io",
+  "gcr.zzh1998.com": "https://gcr.io",
+  "k8s-gcr.zzh1998.com": "https://k8s.gcr.io",
+  "k8s.zzh1998.com": "https://registry.k8s.io",
+  "ghcr.zzh1998.com": "https://ghcr.io",
+  "cloudsmith.zzh1998.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zzh1998.com": dockerHub,
 };
 
 function routeByHosts(host) {
